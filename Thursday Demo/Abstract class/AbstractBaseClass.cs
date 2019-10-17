2f67502c4314af8c978f999ba95b1122341fb588
codycmartin <codycmartin89@gmail.com>
2019-10-17 14:43:17 -0700

    Missing some

---
+++ Thursday Demo/Abstract class/AbstractBaseClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class
+{
+    abstract class AbstractBaseClass
+    {
+        public abstract int AbstractMethod(int num);
+
+    }
+}
---
+++ Thursday Demo/arrayofobjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayofobjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            double v = 456.789;
+            string s = "somestring";
+            MyClass mc = new MyClass();
+
+            Object[] myArray = new object[4];
+            myArray[0] = 1;
+            myArray[1] = "d";
+            myArray[2] = 5;
+            myArray[3] = mc;
+
+            //example of polymorphic
+            foreach(object o in myArray)
+            {
+                Console.WriteLine(o.GetType());
+                Console.WriteLine(o.ToString());
+                Console.WriteLine();
+
+            }
+            
+
+        }
+    }
+}
